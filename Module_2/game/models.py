0a889f18467e7d04f5359c1a9415d0402eec48fe
Anhelina <linaangelinamih@gmail.com>
2025-04-08 17:46:47 +0200

    Создала settings.py и exceptions.py

---
+++ Module_2/game/models.py
@@ -0,0 +1,12 @@
+from .settings import PLAYER_LIVES, ALLOWED_ATTACKS
+from .exceptions import GameOver, EnemyDown
+import random
+
+class Player:
+    name = input("Введите имя игрока")
+    # lives = 
+    score = [0]
+    pass
+
+class Enemy:
+    pass
